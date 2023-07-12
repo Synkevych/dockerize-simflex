f12709ef2fd7ce885b88635a265ca9bc03365e76
Synkevych Roman <sunkevu4@gmail.com>
2023-07-12 22:08:26 +0300

    update input params for simflex

--- calculation/parser.py
+++ calculation/parser.py
@@ -33,9 +33,7 @@ def get_xml_params(file=options_file_path):
   end_date_time = datetime.strptime(end_date_time_str, '%Y-%m-%d %H:%M:%S')
   nx = (xml_root.find('nx').text).split('.')[0]
   ny = (xml_root.find('ny').text).split('.')[0]
-  loutstep = xml_root.find('loutstep').text if xml_root.find(
-      'loutstep') is not None else '3600'
-  min_height = xml_root.find('minheight').text if float(xml_root.find('minheight').text) > 1 else '1'
+
   return {
       'start_date_time': start_date_time,
       'end_date_time': end_date_time,
@@ -45,11 +43,12 @@ def get_xml_params(file=options_file_path):
       'num_y_grid': ny,
       'dx_out': xml_root.find('dlat').text,
       'dy_out': xml_root.find('dlon').text,
-      'minheight': min_height,
+      'minheight': xml_root.find('minheight').text,
       'maxheight': xml_root.find('maxheight').text,
-      'loutstep': loutstep,
+      'loutstep': xml_root.find('loutstep').text,
       'series_id': xml_root.find('id_series').text,
-      'calc_id': xml_root.find('id_calc').text
+      'calc_id': xml_root.find('id_calc').text,
+      'thresh_startb': xml_root.find('thresh_startb').text
   }
 
 
@@ -192,7 +191,7 @@ shr_={date_time:%H},
 sminut_={date_time:%M},
 loutstep_={user_params['loutstep']}, ! 3600 default
 tstart_max_=-9999.0,
-thresh_start_=1.0,
+thresh_start_={user_params['thresh_startb']},
 min_duration_={user_params['loutstep']},
 dlon_={user_params['dy_out']},
 dlat_={user_params['dx_out']},
